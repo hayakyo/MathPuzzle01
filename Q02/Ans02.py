b320418a0eda1b0e111688d8039c4000bbf04d44
hayakyo <34264270+hayakyo@users.noreply.github.com>
2019-05-05 18:28:53 +0900

    first commit

---
+++ Q02/Ans02.py
@@ -0,0 +1,17 @@
+operator_names = ['', '+', '-', '*', '/']
+
+for x in range(1000, 10000):
+    x_str = str(x)
+    for i1 in operator_names:
+        for i2 in operator_names:
+            for i3 in operator_names:
+                cal_str = x_str[0:1] + i1 + x_str[1:2] + i2 + x_str[2:3] + \
+                          i3 + x_str[3:4]
+                if len(cal_str) == 4:
+                    continue
+                try:
+                    y = eval(cal_str)
+                    if x_str[::-1] == str(int(y)):
+                        print(x_str, cal_str, y)
+                except:
+                    continue
