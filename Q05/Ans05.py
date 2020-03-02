d27b6685b052578ac3805ed36f82f5224497a5d7
hayakyo <34264270+hayakyo@users.noreply.github.com>
2020-03-02 18:59:15 +0900

    New file add Question05

---
+++ Q05/Ans05.py
@@ -0,0 +1,12 @@
+import itertools
+
+coinType = [0, 10, 50, 100, 500]
+count = 0
+
+for i in itertools.combinations_with_replacement(coinType, r=15):
+    if sum(i) == 1000:
+        print(i)
+        count += 1
+
+print(count)
+
